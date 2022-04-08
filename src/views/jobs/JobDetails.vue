8d5bcbb4eeb5abe5b8458b515813de45d06878ab
Jan Mašlej <102227021+Jenito4@users.noreply.github.com>
2022-04-08 15:08:34 +0200

    Update JobDetails.vue

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,11 @@
 <template>
 <div v-if="job">
   <h1>{{job.title}}</h1>
-  <p>The job id is {{id}}</p> 
+  <p>ID práce je {{id}}</p> 
   <p>{{job.details}}</p>
 </div>
 <div v-else>
-  <p>Loading job details...</p>
+  <p>Načítání detailů...</p>
 </div>
 </template>
 
